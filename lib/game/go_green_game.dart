af37b699c00329d621df0b023c4f04c942cc6655
Yuvanraj <itsmeyuvanraj@gmail.com>
2024-04-03 00:09:46 +0530

    Gesture Move Complete

--- lib/game/go_green_game.dart
+++ lib/game/go_green_game.dart
@@ -1,12 +1,12 @@
-import 'dart:async';
-
 import 'package:flame/camera.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:game_learn/constants.dart';
 import 'package:game_learn/game/go_green_world.dart';
 
-class GoGreenGame extends FlameGame {
+class GoGreenGame extends FlameGame<GoGreenWorld> with HorizontalDragDetector,KeyboardEvents {
   GoGreenGame() : super(
       world: GoGreenWorld(),
       camera: CameraComponent.withFixedResolution(
@@ -19,5 +19,28 @@ class GoGreenGame extends FlameGame {
   @override
   Color backgroundColor(){
     return Colors.yellow;
+  }
+
+  @override
+  void onHorizontalDragUpdate(DragUpdateInfo info) {
+    super.onHorizontalDragUpdate(info);
+    world.player.move(info.delta.global.x);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    const double moveSpeed = 55.0;
+
+    if(event is KeyDownEvent){
+      if(keysPressed.contains(LogicalKeyboardKey.arrowRight)){
+        world.player.move(moveSpeed);
+        return KeyEventResult.handled;
+      } else if(keysPressed.contains(LogicalKeyboardKey.arrowLeft)){
+        world.player.move(-moveSpeed);
+        return KeyEventResult.handled;
+      }
+    }
+    return KeyEventResult.ignored;
   }
 }
--- lib/game/go_green_world.dart
+++ lib/game/go_green_world.dart
@@ -5,10 +5,12 @@ import 'package:game_learn/game/go_green_game.dart';
 import 'package:game_learn/player.dart';
 
 class   GoGreenWorld extends World with HasGameRef<GoGreenGame> {
+  late final Player player;
     @override
   FutureOr<void> onLoad() {
       // world means that whole game -  like say game world
-      add(Player());
+      player = Player();
+      add(player);
 
   }
 }
